dcb5b35867ac23de1ebe131e7c1761f5bb3ceb90
ZahwaAnritia <anritia15@gmail.com>
2024-05-06 11:09:59 +0700

    add modifier private dan public untuk kelas mahasiswa

---
+++ FriendFunction/FriendFunction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private:
+	string nama;
+public:
+	friend void setNama(mahasiswa& a, string);
+};
+int main()
+{
+   
+}
