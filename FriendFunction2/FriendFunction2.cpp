36ba0c19a3c18d7055fb553f50bf23f4f7a041d2
ZahwaAnritia <anritia15@gmail.com>
2024-05-06 11:25:42 +0700

    add void manusia dan int main

--- FriendFunction2/FriendFunction2.cpp
+++ FriendFunction2/FriendFunction2.cpp
@@ -2,17 +2,34 @@
 using namespace std;
 
 class pelajar;
-class mahaisswa
+class manusia
 {
 public:
 	void showNilaiPelajar(pelajar& x);
 };
 
+class pelajar
+{
+private:
+	int nilai;
+
+public:
+	pelajar() { nilai = 100; }
+	friend void manusia:: showNilaiPelajar(pelajar& x);
+};
+
+void manusia::showNilaiPelajar(pelajar& x)
+{
+	std::cout << x.nilai;
+}
 
 
 int main()
 {
-   
+	manusia budi;
+	pelajar pbudi;
+	budi.showNilaiPelajar(pbudi);
+	return 0;
 }
 
 
